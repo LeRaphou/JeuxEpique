4de94fae794c3b4c4399ebd42a1a3c4d15d7c87d
LeRaphouu <raphael.bonnet@ynov.com>
2026-02-24 10:05:34 +0100

    feat: page 404 done

---
+++ views/404.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="fr">
+<head><meta charset="utf-8"><title><?= $title = "Jeux Epique";
+        htmlspecialchars($title) ?></title></head>
+<body>
+<h1>404</h1>
+<p><a href="/contact">Contact</a></p>
+</body>
+</html>
