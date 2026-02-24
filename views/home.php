ef4dc80190c761098dfaf8866c9de270fedc0779
LeRaphouu <raphael.bonnet@ynov.com>
2026-02-24 10:07:40 +0100

    feat: home page php added

---
+++ views/home.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="fr">
+<head><meta charset="utf-8"><title><?= $title = "Jeux Epique";
+        htmlspecialchars($title) ?></title></head>
+<body>
+<h1>Accueil</h1>
+<p><a href="/contact">Contact</a></p>
+</body>
+</html>
