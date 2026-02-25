5b2654ed50f5efb013a9a1aca0cb55e5060f106b
cnoah <noah.charrinbourrat@ynov.com>
2026-02-25 23:44:32 +0100

    feat: add app.css stylesheet to home.php for improved styling

--- views/home.php
+++ views/home.php
@@ -48,6 +48,7 @@ $currentSlide = 0;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/app.css"/>
     <link rel="stylesheet" href="/assets/css/home.css"/>
 </head>
 <body>
