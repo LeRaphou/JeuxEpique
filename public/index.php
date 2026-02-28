6912a8652e4b36b291f1f228cad61b1751e60b8a
cnoah <noah.charrinbourrat@ynov.com>
2026-02-28 18:22:46 +0100

    feat: implement database connection using environment variables

--- public/index.php
+++ public/index.php
@@ -1,5 +1,28 @@
 <?php
 declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+
+$uri = "mysql://" . $_ENV["DB_USER"] .":" . $_ENV["DB_PASSWORD"] . "@" . $_ENV["DB_HOST"] . ":" . $_ENV["DB_PORT"] . "/" . $_ENV["DB_NAME"] ."?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+$conn = "mysql:";
+$conn .= "host=" . $_ENV["DB_HOST"];
+$conn .= ";port=" . $_ENV["DB_PORT"];
+$conn .= ";dbname=jeux-epiques";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/';
 
