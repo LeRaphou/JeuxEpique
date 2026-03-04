0274040cb7dea127b59fe8068158262d05f65d57
cnoah <noah.charrinbourrat@ynov.com>
2026-03-04 22:50:50 +0100

    refactor: update AdminActions to use Auth for user role checks and improve parameter handling

--- src/AdminActions.php
+++ src/AdminActions.php
@@ -2,36 +2,39 @@
 
 namespace App;
 
+use Auth;
 use PDO;
 
 class AdminActions
 {
-    private int $user;
-    private PDO $db;
 
-    public function __construct($user, $db)
+
+    public function __construct(
+        private int $userId,
+        private Auth $auth,
+        private PDO $db,
+    )
     {
-        $this->user = $user;
-        $this->db = $db;
+
     }
 
-    public function deleteUser($userId)
+    public function deleteUser(int $otherUserId): void
     {
-        if ($this->user->getUserRole() !== 'admin') {
+        if ($this->auth->isAdmin($this->userId) === false) {
             throw new \Exception("Unauthorized");
         }
 
         $stmt = $this->db->prepare('DELETE FROM Users WHERE id = :id');
-        $stmt->execute(['id' => $userId]);
+        $stmt->execute(['id' => $otherUserId]);
     }
 
-    public function promoteUser($userId)
+    public function promoteUser(int $otherUserId): void
     {
-        if ($this->user->getUserRole() !== 'admin') {
+        if ($this->auth->isAdmin($this->userId) === false) {
             throw new \Exception("Unauthorized");
         }
 
         $stmt = $this->db->prepare('UPDATE Users SET admin = 1 WHERE id = :id');
-        $stmt->execute(['id' => $userId]);
+        $stmt->execute(['id' => $otherUserId]);
     }
 }
